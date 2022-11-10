baae7f45e2d6840f34b3ba1c450210466e66ab10
Papa-DMan <66575559+Papa-DMan@users.noreply.github.com>
2022-11-10 13:41:01 -0500

    .gitignore

--- src/main/java/com/mahaney/cis296project3/HelloController.java
+++ src/main/java/com/mahaney/cis296project3/HelloController.java
@@ -1,14 +1,25 @@
 package com.mahaney.cis296project3;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    private Label stackDisplay;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private void calcInputCallback(ActionEvent e) {
+        final Node source = (Node) e.getSource();
+        if (source instanceof Button) {
+            if (((Button) source).getText().contains("⎵")) {
+                stackDisplay.setText(stackDisplay.getText() + " ");
+            } else {
+                stackDisplay.setText(stackDisplay.getText() + ((Button) source).getText());
+            }
+        }
     }
+
 }
