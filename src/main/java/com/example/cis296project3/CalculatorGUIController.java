9425838d6641300995fa97a30f52f66894119d13
Papa-DMan <danjmahaney@gmail.com>
2022-11-22 15:31:48 -0500

    Implemented calculator functionality and sqashed some bugs

--- src/main/java/com/example/cis296project3/CalculatorGUIController.java
+++ src/main/java/com/example/cis296project3/CalculatorGUIController.java
@@ -6,20 +6,116 @@ import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.text.Text;
 
+import java.util.EmptyStackException;
+import java.util.Stack;
+
 public class CalculatorGUIController {
-    @FXML
-    private Text stackDisplay;
+	@FXML
+	private Text stackDisplay;				//display text
+	private boolean isClear = true;			//flag for just cleared input
 
-    @FXML
-    private void calcInputCallback(ActionEvent e) {
-        final Node source = (Node) e.getSource();
-        if (source instanceof Button) {
-            if (((Button) source).getText().contains("⎵")) {
-                stackDisplay.setText(stackDisplay.getText() + " ");
-            } else {
-                stackDisplay.setText(stackDisplay.getText() + ((Button) source).getText());
-            }
-        }
-    }
+	private String input = "";				//string to track input
 
+	private Stack<String> stringStack = new Stack<>(); //stack for calculation
+
+
+	@FXML
+	private void calcInputCallback(ActionEvent e) {
+		final Node source = (Node) e.getSource();
+		if (source instanceof Button) {
+			try {
+				switch (((Button) source).getText()) {
+					//Case for space input
+					case "⎵": {
+						input += " ";
+						stackDisplay.setText(stackDisplay.getText() + " ");
+						break;
+					}
+					//Case for clear input
+					case "C": {
+						input = "";
+						stackDisplay.setText("0");
+						isClear = true;
+						break;
+					}
+					//Case for equals input
+					case "=": {
+						String inputs[];
+						isClear = true;
+						inputs = input.split(" ");
+						//Runs through user input and calculates the result
+						for (int i = 0; i < inputs.length; i++) {
+							switch (inputs[i]) {
+								//Case for addition input
+								case "+": {
+									double x = Double.parseDouble(stringStack.pop());
+									double y = Double.parseDouble(stringStack.pop());
+									double res = x + y;
+									stringStack.push("" + res);
+									break;
+								}
+								//Case for subtraction input
+								case "–": {
+									double x = Double.parseDouble(stringStack.pop());
+									double y = Double.parseDouble(stringStack.pop());
+									double res = y - x;
+									stringStack.push("" + res);
+									break;
+
+								}
+								//Case for multiplication
+								case "x": {
+									double x = Double.parseDouble(stringStack.pop());
+									double y = Double.parseDouble(stringStack.pop());
+									double res = x * y;
+									stringStack.push("" + res);
+
+									break;
+								}
+								//Case for division
+								case "/": {
+									double x = Double.parseDouble(stringStack.pop());
+									if (x == 0) {
+										throw new EmptyStackException();
+									}
+									double y = Double.parseDouble(stringStack.pop());
+									double res = y / x;
+									stringStack.push("" + res);
+									break;
+								}
+								//Default case; pushes doubles to the stack
+								default: {
+									stringStack.push(inputs[i]);
+									break;
+								}
+							}
+						}
+						//remove decimal point if zero
+						String result = stringStack.pop();
+						if (result.endsWith(".0")) {
+							result = result.substring(0, result.length() - 2);
+						}
+						stackDisplay.setText("" + result);
+						stringStack.clear();
+						input = "";
+						break;
+					}
+					default: {
+						if (isClear) { //if isClear flag is set the next input overrides what is currently in the display window
+							stackDisplay.setText(((Button) source).getText());
+							isClear = false;
+						} else {
+							stackDisplay.setText(stackDisplay.getText() + ((Button) source).getText());
+						}
+						input += ((Button) source).getText();
+						break;
+					}
+				}
+			} catch (EmptyStackException emptyStackException) { //catch when the stack is empty mid-operation and display error
+				stackDisplay.setText("Error");
+				stringStack.clear();
+				input = "";
+			}
+		}
+	}
 }
--- src/main/java/com/example/cis296project3/CalculatorProject.java
+++ src/main/java/com/example/cis296project3/CalculatorProject.java
@@ -1,8 +1,13 @@
 package com.example.cis296project3;
 
 import javafx.application.Application;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -11,10 +16,10 @@ public class CalculatorProject extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(CalculatorProject.class.getResource("Calculator.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 300, 650);
-        String style = getClass().getResource("styles.css").toExternalForm();
-        scene.getStylesheets().add(style);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 250, 445);
+        String style = getClass().getResource("styles.css").toExternalForm(); // load css
+        scene.getStylesheets().add(style);   //add css as a stylesheet
+        stage.setTitle("Calculator");
         stage.setScene(scene);
         stage.setResizable(false);
         stage.show();
